0970b0bf2435676d15028c78ff9657fa0dbcaa1e
Wen <409053122@qq.com>
2023-04-07 15:30:00 +0800

    基础组件

---
+++ mini-im-core/src/main/java/com/wen/open/miniim/StartApplication.java
@@ -0,0 +1,15 @@
+package com.wen.open.miniim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wen
+ * @date 2023/4/7 14:54
+ */
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
---
+++ mini-im-core/src/main/java/com/wen/open/miniim/context/GlobalEnvironmentContext.java
@@ -0,0 +1,15 @@
+package com.wen.open.miniim.context;
+
+import io.netty.channel.socket.DatagramPacket;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Wen
+ * @date 2023/4/7 15:04
+ */
+public class GlobalEnvironmentContext {
+
+    /** 客户端的地址列表 */
+    public static ConcurrentHashMap<String, DatagramPacket> cliMap = new ConcurrentHashMap<>();
+}
